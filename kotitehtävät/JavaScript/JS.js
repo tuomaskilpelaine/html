636e96b7db7b322e01b2f73f1cdf102282ce6815
tuomaskilpelaine <114050494+tuomaskilpelaine@users.noreply.github.com>
2023-06-06 10:58:57 +0300

    .

---
+++ kotitehtävät/JavaScript/JS.js
@@ -0,0 +1,12 @@
+var me= "tuomas"
+var profession= "ohjelmistokehittäjä"
+var education= "perustutkinto"
+var hometown= "Lohja"
+
+var whoami = "lohja "+ hometown +
+
+alert(whoami);
+
+console.log(whoami);
+console.error("huomio tämä ei ole totta:"+ whoami);
+console.warn("onkohan varmasti kotikunta"+hometown);
